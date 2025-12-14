35aa4aa7915c39a2a6647985c3644f96b995d19f
asepjamaludinn <asepjamaludinn24@gmail.com>
2025-12-15 00:24:57 +0700

    feat: generate resource setup - generate resources for users, auth, materials, stocks, request, distributions - implement create materials service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MaterialsModule } from './materials/materials.module';
+import { StocksModule } from './stocks/stocks.module';
+import { RequestsModule } from './requests/requests.module';
+import { DistributionsModule } from './distributions/distributions.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, MaterialsModule, StocksModule, RequestsModule, DistributionsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/materials/materials.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMaterialDto } from './dto/create-material.dto';
+import { UpdateMaterialDto } from './dto/update-material.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MaterialsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createMaterialDto: CreateMaterialDto) {
+    return this.prisma.material.create({
+      data: createMaterialDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.material.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.material.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateMaterialDto: UpdateMaterialDto) {
+    return this.prisma.material.update({
+      where: { id },
+      data: updateMaterialDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.material.delete({ where: { id } });
+  }
+}
